16e10c753a099b82db70d0ebd8ea0212933eada4
Nicha Chomsawat <nicha.chom@ku.th>
2025-07-10 15:10:10 +0700

    lab01.py

---
+++ assignments/week01/lap01.py
@@ -0,0 +1,21 @@
+# Exercise 1: Personal Profile Creator
+print("=== Personal Profile Creator ===")
+# Create a program that asks for:
+# - Full name
+# - Age
+# - Email
+# - Phone number
+# - Favorite hobby
+# Then display it as a profile
+
+# Write your solution here: 
+fullname = input ("Enter Full Name : ")
+age = int(input("Enter Age : "))
+email = input("Enter Email : ")
+phone = int(input("Enter Phone Number : "))
+favoritehobby = input("Enter Favorite hobby : ")
+print("Name ; ",fullname)
+print("age : ",age)
+print("email: ",email)
+print("phone: ",phone)
+print("favoritehobby: ",favoritehobby)
